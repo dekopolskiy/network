8bd62e6509e6cf6deee5117cf64afa847d1813b8
dmitridecopolski <dmitridecopolski@yandex.ru>
2021-04-27 21:58:31 +0300

    typscript AC

--- src/redux/actions_creator.ts
+++ src/redux/actions_creator.ts
@@ -1,34 +1,51 @@
 import { SET_AUTHORIZE, SET_CAPTCHA, SET_USER } from "./reducers/current_user_";
 import { IS_LOAD } from "./reducers/loading_";
 import { SET_ERROR } from "./reducers/error_"
-import { SET_PROFILE, SET_AVATAR, RESET_PROFILE } from "./reducers/profile_";
+import { SET_PROFILE, SET_AVATAR, RESET_PROFILE, ProfileType } from "./reducers/profile_";
 import { SET_STATUS } from "./reducers/status_";
 import { ENABLE_PROGRESS_SUBSCRIBE, SET_FOLLOW, DISABLE_PROGRESS_SUBSCRIBE, SET_PAGE_INFO, SET_UNFOLLOW, SET_USERS } from "./reducers/users_";
 
-const SetUserType = typeof SET_USER;
-export const set_user = (payload: any) => {
-  debugger
-  return  {
-  type: SetUserType,
+type SetUserType = {
+  type: typeof SET_USER,
+  payload: any,
+};
+export const set_user = (payload: any): SetUserType => ({
+  type: SET_USER,
   payload
-}}
+})
 
-export const set_load = (payload: any) => ({
+type SetLoadType = {
+  type: typeof IS_LOAD,
+  payload: boolean, 
+}
+export const set_load = (payload: boolean): SetLoadType => ({
   type: IS_LOAD,
   payload,
 });
 
-export const set_error = (payload: any) => ({
+type SetErrorType = {
+  type: typeof SET_ERROR,
+  payload: string
+}
+export const set_error = (payload: string): SetErrorType => ({
   type: SET_ERROR,
   payload
 })
 
-export const set_profile = (payload: any) => ({
+type SetProfileType = {
+  type: typeof SET_PROFILE,
+  payload: ProfileType,
+}
+export const set_profile = (payload: ProfileType): SetProfileType => ({
   type: SET_PROFILE,
   payload
 })
 
-export const set_status = (payload: any) => ({
+type SetStatusType = {
+  type: typeof SET_STATUS,
+  payload: string
+}
+export const set_status = (payload: string):SetStatusType => ({
   type: SET_STATUS,
   payload
 })
